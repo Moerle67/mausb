7f6ed2e0fa5e8d5ad4290cfe540ac7a8eb6fcb8f
Ingo Mörl <i.moerl@bfw-schoemberg.de>
2026-01-14 11:40:17 +0100

    Standort

--- stammdaten/admin.py
+++ stammdaten/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+from .models import Ausbilder, Standort
 # Register your models here.
+
+admin.site.register(Standort)
+
+@admin.register(Ausbilder)
+class AusbilderAdmin(admin.ModelAdmin):
+    list_filter = ['activ']
+#    search_fields = ['user_name']
--- stammdaten/models.py
+++ stammdaten/models.py
@@ -1,3 +1,40 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.urls import reverse
 
-# Create your models here.
+# Create your models here
+
+class Ausbilder(models.Model):
+    user = models.OneToOneField(User, verbose_name=("SystemUser"), on_delete=models.RESTRICT)
+    short = models.CharField(("Kürzel"), max_length=10)
+    activ = models.BooleanField(("Aktiv"), default=True)
+    bg_color = models.CharField(("Hintergrundfarbe"), max_length=50, default="DodgerBlue")
+    fg_color = models.CharField("Schriftfarbe", max_length=50, default="white")
+    comment = models.TextField(("Kommentar"), blank=True, null=True)
+
+    class Meta:
+        verbose_name = "Ausbilder"
+        verbose_name_plural = "Ausbilder"
+
+    def __str__(self):
+        status = "Aktiv" if self.activ else "Inaktiv"
+        return f"{self.user.first_name} {self.user.last_name} ({self.short}/{status})"
+
+    def get_absolute_url(self):
+        return reverse("Ausbilder_detail", kwargs={"pk": self.pk})
+
+class Standort(models.Model):
+    short = models.CharField(("Kürzel"), max_length=10)
+    ort = models.CharField("Ort", max_length=50)
+    description = models.TextField(("Beschreibung"), null=True, blank=True)
+    activ = models.BooleanField("Aktiv", default=True)
+    
+    class Meta:
+        verbose_name = "Standort"
+        verbose_name_plural = "Standorte"
+
+    def __str__(self):
+        return f"{self.short} - {self.ort}"
+
+    def get_absolute_url(self):
+        return reverse("Standort_detail", kwargs={"pk": self.pk})
