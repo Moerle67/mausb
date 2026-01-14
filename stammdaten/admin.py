3125966bde768dbabc22152cbc1204491f11a346
Ingo Mörl <i.moerl@bfw-schoemberg.de>
2026-01-14 13:29:58 +0100

    Beruf

--- stammdaten/admin.py
+++ stammdaten/admin.py
@@ -1,11 +1,16 @@
 from django.contrib import admin
 
-from .models import Ausbilder, Standort
+from .models import Ausbilder, Standort, Team, Beruf
 # Register your models here.
 
 admin.site.register(Standort)
+admin.site.register(Beruf)
 
 @admin.register(Ausbilder)
 class AusbilderAdmin(admin.ModelAdmin):
     list_filter = ['activ']
 #    search_fields = ['user_name']
+
+@admin.register(Team)
+class TeamAdmin(admin.ModelAdmin):
+    filter_horizontal = ['aubi']
--- stammdaten/models.py
+++ stammdaten/models.py
@@ -38,3 +38,37 @@ class Standort(models.Model):
 
     def get_absolute_url(self):
         return reverse("Standort_detail", kwargs={"pk": self.pk})
+
+class Team(models.Model):
+    name = models.CharField(("Bezeichnung"), max_length=50)
+    short = models.CharField(("Kürzel"), max_length=10)
+    aubi = models.ManyToManyField(Ausbilder, verbose_name=("Ausbilder"))
+    standort = models.ForeignKey(Standort, verbose_name=("Standort"), on_delete=models.CASCADE)
+    head = models.ForeignKey(Ausbilder, verbose_name=("Teamleiter"), related_name="Teamleiter", on_delete=models.RESTRICT, null=True, blank=True)
+    activ = models.BooleanField(("aktiv"), default=True)
+
+    class Meta:
+        verbose_name = ("Team")
+        verbose_name_plural = ("Teams")
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse("Team_detail", kwargs={"pk": self.pk})
+
+class Beruf(models.Model):
+    short = models.CharField(("Kürzel"), max_length=10)
+    name = models.CharField(("Bezeichnung"), max_length=50)
+    activ = models.BooleanField(("Aktiv"), default=True)
+
+    class Meta:
+        verbose_name = "Beruf"
+        verbose_name_plural = "Berufe"
+
+    def __str__(self):
+        return f"{self.name} ({self.short})"
+
+    def get_absolute_url(self):
+        return reverse("Beruf_detail", kwargs={"pk": self.pk})
+
