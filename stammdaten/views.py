65fae32585966a84bd1f8224e717ea5b1d8e0aeb
Ingo Mörl <i.moerl@bfw-schoemberg.de>
2026-01-14 16:09:42 +0100

    Login

--- stammdaten/views.py
+++ stammdaten/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.contrib.auth.views import LoginView
+from django.urls import reverse_lazy
+from django.contrib import messages
+from django.http import HttpResponse
 
 # Create your views here.
+
+class MyLoginView(LoginView):
+    redirect_authenticated_user = True
+    
+    def get_success_url(self):
+        return reverse_lazy('start') 
+    
+    def form_invalid(self, form):
+        messages.error(self.request,'Invalid username or password')
+        return self.render_to_response(self.get_context_data(form=form))
+
+def start(request):
+    return HttpResponse("Startseite")
