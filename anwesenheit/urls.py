d819404341032d7eecfbc78d4c493f6de3f640b4
Ingo Mörl <i.moerl@bfw-schoemberg.de>
2026-01-19 16:44:30 +0100

    Anwesenheit

--- anwesenheit/urls.py
+++ anwesenheit/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import start
+from .views import start, anw_group, anw_detail
 
 app_name = "anw"
     
 urlpatterns = [
     path('', start ,name='start'),
+    path('<int:id>', anw_group ,name='anw_g'),
+    path('detail/<int:id>', anw_detail ,name='anw_detail'),
 ]
--- anwesenheit/views.py
+++ anwesenheit/views.py
@@ -1,11 +1,53 @@
-from django.shortcuts import redirect, render
+from django.shortcuts import redirect, get_object_or_404, render
+
+from stammdaten.models import Team, Gruppe, Teilnehmer
+import stammdaten.classForm as cform 
 
 # Create your views here.
 
 def start(request):
+    # Erstaufruf zur Auswahl des Teams
+    teams = Team.objects.filter(activ = True)
+    # Nur ein Team?
+    if len(teams)==1 :
+        return redirect(f"/anw/{teams[0].id}")
+    
+    teams = cform.FormAuswahl("Teams", Team, leerzeile=True, aktiv=True, onclick='anw_group(this.value)')
+    
     content = {
         'cont': 'anw:start',
+        'teams': teams,
     }
+    return render(request,"anwesenheit/anw_team.html", content)
 
-    return render(request,"anwesenheit/start.html", content)
+def anw_group(request, id):
+    team = get_object_or_404(Team, id=id)
+    teams = cform.FormAuswahl("Teams", Team, leerzeile=True, aktiv=True, value=team.id,  onclick='anw_group(this.value)')
+    groups = Gruppe.objects.filter(activ=True, team=team)
+    select_groups = cform.FormAuswahl("Gruppe", daten=groups, leerzeile=True, onclick='anw_detail(this.value)')
+    content = {
+        'cont': 'anw:anw_g',
+        'team': team,
+        'teams': teams,
+        'groups': select_groups,
+    }
+    return render(request,"anwesenheit/anw_group.html", content)
 
+def anw_detail(request, id, date=-1):
+    gruppe = get_object_or_404(Gruppe, id=id)
+    team = gruppe.team
+    groups = Gruppe.objects.filter(activ=True, team=team)
+
+    teams = cform.FormAuswahl("Teams", Team, leerzeile=True, aktiv=True, value=team.id,  onclick='anw_group(this.value)')
+    select_groups = cform.FormAuswahl("Gruppe", daten=groups, leerzeile=True, value=id, onclick='anw_detail(this.value)')
+
+    tn_group = Teilnehmer.objects.filter(activ=True, group=gruppe)
+    print(tn_group)
+    content = {
+        'cont': 'anw:start',
+        'teams': teams,
+        'groups': select_groups,
+        'teilnehmer': tn_group,
+        
+    }
+    return render(request,"anwesenheit/anw_detail.html", content)
