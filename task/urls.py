a46d45bb9c9e6be8dbb78c976dbb25a96d1e96a3
Ingo Mörl <ingo.moerl@posteo.de>
2026-03-29 19:54:48 +0200

    Task

--- task/urls.py
+++ task/urls.py
@@ -6,5 +6,6 @@ app_name = "task"
 urlpatterns = [
     path('', views.start, name = "start"),                                  # Start ohne Paramter
     path('get_task_form', views.get_task_form, name = "get_task_form"),     # Daten für Formular add Task holen
+    path('save_task_form', views.save_task_form, name = "save_task_form"),     # Daten für Formular add Task holen
         
 ]
--- task/views.py
+++ task/views.py
@@ -25,13 +25,29 @@ def start(request):
     return render(request, 'task/task_start.html', content)
 
 def get_task_form(request):
+    # select für Befreich generieren
     lst_bereich = Bereich.objects.all()
-    str_bereich = '<select class="form-select" id="task-bereich">'
+    str_bereich = ""
     for zeile in lst_bereich:
         str_bereich += f'<option value = "{zeile.id}">{zeile.name}</option>'
-    str_bereich += '</select>'
+    
+    # Liste möglicher Verantwortlicher generieren
     lst_verant = User.objects.filter(is_active = True)
+    str_verant = ""
+    for zeile in lst_verant:
+        str_verant += f'<option value = "{zeile.id}">{zeile.last_name}, {zeile.first_name}</option>'
+
     answer = {
-        'bereich': str_bereich,
+        'bereich'   : str_bereich,
+        'verant'    : str_verant,
+    }
+    return HttpResponse(json.dumps(answer), content_type="application/json")
+
+def save_task_form(request):
+    zykl = False if request.POST['zykl'] == "false" else True
+    aufgabe = request.POST['aufgabe']
+    print(zykl, aufgabe)
+    answer = {
+        'error'   : False,
     }
     return HttpResponse(json.dumps(answer), content_type="application/json")
