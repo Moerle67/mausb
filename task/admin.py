0feb261b3dc87a04327408bbcb6013951854c84e
Ingo Mörl <i.moerl@bfw-schoemberg.de>
2026-04-08 09:57:56 +0200

    Filter

--- task/admin.py
+++ task/admin.py
@@ -4,5 +4,11 @@ from .models import Aufgabe, Bereich
 
 # Register your models here.
 
-admin.site.register(Aufgabe)
+# admin.site.register(Aufgabe)
 admin.site.register(Bereich)
+
+
+@admin.register(Aufgabe)
+class AufgabeAdmin(admin.ModelAdmin):
+    list_filter         = ['verantwortlich', 'aktuell', 'aktiv']
+    search_fields = ['ueber', 'inhalt']
