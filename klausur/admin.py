4b0f677b20e264bb9a52fd7ec864c3a6db59593e
Ingo Mörl <i.moerl@bfw-schoemberg.de>
2026-07-09 08:36:57 +0200

    del Klausur

--- klausur/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-from .models import Klausur, Frage
-
-# Register your models here.
-
-admin.site.register(Klausur)
-admin.site.register(Frage)
--- klausur/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = "klausur"
-
-urlpatterns = [
-    path('', views.start, name = "start"),                      # Start ohne Paramter    
-]
