2da506651545ed21e7a2bd6e3c6956e4db0ddf14
Ingo Mörl <i.moerl@bfw-schoemberg.de>
2026-08-31 12:59:29 +0200

    Klausur Datenbanken

--- kklausur/admin.py
+++ kklausur/admin.py
@@ -4,3 +4,9 @@ from .models import *
 # Register your models here.
 
 admin.site.register(Frage)
+admin.site.register(Klausur)
+# admin.site.register(KlausurFrage)
+
+@admin.register(KlausurFrage)
+class KlausurFrageAdmin(admin.ModelAdmin):
+    list_filter = ['klausur']
--- kklausur/models.py
+++ kklausur/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 from lehrplan.models import Ausbildungseinheit
+from stammdaten.models import Gruppe
 
 # Create your models here.
 
@@ -27,3 +28,44 @@ class Frage(models.Model):
 
     #def get_absolute_url(self):
     #    return reverse("Fragen_detail", kwargs={"pk": self.pk})
+
+class Klausur(models.Model):
+    title       = models.CharField(("Überschrift"), max_length=50)
+    gruppe      = models.ForeignKey(Gruppe, verbose_name="Gruppe", on_delete=models.CASCADE)
+    thema       = models.ForeignKey(Ausbildungseinheit, verbose_name="Thema", on_delete=models.SET_NULL, null=True)
+    datum       = models.DateTimeField("Termin", auto_now=False, auto_now_add=False)
+    erledigt    = models.BooleanField("abgeschlossen", default = False)
+    comment     = models.TextField("Kommentar", blank=True, null=True)
+
+    class Meta:
+        verbose_name            = "Klausur"
+        verbose_name_plural     = "Klausuren"
+        ordering                = ["gruppe", "-datum"]
+
+    def __str__(self):
+        return f"{self.gruppe} / {self.title}"
+    
+    #    def get_absolute_url(self):
+    #        return reverse("Klausur_detail", kwargs={"pk": self.pk})
+    
+class KlausurFrage(models.Model):
+    klausur     = models.ForeignKey(Klausur, verbose_name="Klausur", on_delete=models.CASCADE)
+    frage       = models.ForeignKey(Frage, verbose_name="Frage", on_delete=models.CASCADE)
+    position    = models.IntegerField("Position", default=0)
+    startnp     = models.BooleanField("Start mit neuer Seite", default=False)
+
+    class Meta:
+        verbose_name        = "KlausurFrage"
+        verbose_name_plural = "KlausurFrage"
+        ordering            = ["klausur", "position"]
+
+    def __str__(self):
+        return f"{self.klausur} - {self.frage} ({self.position})"
+
+    @property
+    def get_erledigt(self):
+        return self.klausur.erledigt  
+
+    #def get_absolute_url(self):
+    #    return reverse("KlausurFrage_detail", kwargs={"pk": self.pk})
+    
