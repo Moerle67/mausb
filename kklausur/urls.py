2c7d63da7312c99340a36adfd311fbc5ca29f4a1
Ingo Mörl <i.moerl@bfw-schoemberg.de>
2026-09-07 11:41:02 +0200

    Klausur Details

--- kklausur/urls.py
+++ kklausur/urls.py
@@ -9,7 +9,9 @@ urlpatterns = [
     path('', start, name='start'),                                              # Start Klausuren Auswahl Gruppe
     path('ausw_klausur/<int:gruppe>', ausw_klausur, name='ausw_klausur'),       # Start 2 Auswahl Klausur
     path('new_klausur/<int:gruppe>', new_klausur, name='ausw_klausur'),         # Start 2 Auswahl Klausur
-    path('add_klas/<int:team>', add_klas, name='add_klas'),                     # Klausur zufügen
+    path('detail_klausur/<int:klausur>', detail_klausur, name='detail_klausur'), # Start 2 Auswahl Klausur
+
+    path('chg_klausur_title>', chg_klausur_title, name='chg_klausur_title'),   # Neuen Titel in Klausur speichern
     path('gen_pdf/<int:klausur>', gen_pdf, name='gen_pdf'),                     # PDF generieren     
 ]
 
